7e4e04d258b6d8458d60fe4c5ad8d669891f53d6
Diego <diegovillarreal123@gmail.com>
2021-07-09 13:47:08 -0500

    added packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SavedBooks from "./pages/SavedBooks";
+import Books from "./pages/Books";
+import Nav from "./components/Nav/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <div>
+    <Nav />
+      <Switch>
+       <Route exact path="/" component={Books}>
+      </Route>
+       <Route exact path="/savedbooks" component={SavedBooks}>
+       </Route>
+      </Switch>
+      </div>
+      </Router>
   );
 }
 
